74b2f4f73d2dce6fc9453b6c2917ca3a30cbc290
cassandrayuen <yuen.cassandra.i@gmail.com>
2024-10-30 11:56:28 +1100

    fix #6 implement basic functionality

---
+++ R/get_times_daily.R
@@ -0,0 +1,22 @@
+get_times_daily <- function(station_id = 300,
+                      variable = "pm25",
+                      username = api_key$username,
+                      password = api_key$password) {
+  # Retrieve time range for station
+  req <- httr::GET("https://napmd.cloud.car-dat.org/get_times_daily",query=list(station_id=station_id,
+                                                           variable=variable,
+                                                           username=username, 
+                                                           password=password,
+                                                           datatype="JSON"))
+  
+  # catch if error
+  if (req$status_code != 200) {
+    parse_data <- httr::content(req, as = "parsed")
+    warning(sprintf("%s: %s", req$status_code, parse_data$error))
+    return(NULL)
+  }
+  
+  req <- httr::content(req,as="parsed")
+  st_times <- data.table::rbindlist(req,fill=TRUE)
+  return(st_times)
+}
---
+++ R/get_variables_daily.R
@@ -0,0 +1,22 @@
+get_variables_daily <- function(
+    station_id = 300,
+    username = api_key$username,
+    password = api_key$password
+){
+  
+  req <- httr::GET("https://napmd.cloud.car-dat.org/get_variables_daily",query=list(station_id=station_id,
+                                                                             username=username, 
+                                                                             password=password,
+                                                                             datatype="JSON"))
+  
+  # catch if error
+  if(req$status_code != 200){
+    parse_data <- httr::content(req,as="parsed")
+    warning(sprintf("%s: %s", req$status_code, parse_data$error))
+    return(NULL)
+  }
+  
+  req <- httr::content(req,as="parsed")
+  st_vars <- data.table::rbindlist(req,fill=TRUE)
+  return(st_vars)
+}
